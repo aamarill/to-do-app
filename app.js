8ef07d3b004e676ae1e19d61eaad181a3aa81e31
Adan Amarillas <aamarill.engr@gmail.com>
2017-09-22 11:05:17 -0700

    Add delete feature

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-  function onReady() {
+function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList    = document.getElementById('toDoList');
-
+  var idNumber = 1;
   addToDoForm.addEventListener('submit', (event) => {
 
     event.preventDefault();
@@ -12,12 +12,24 @@
 
     // Create a new li.
     let newLi = document.createElement('li');
+    // Set the li id.
+    newLi.id = 'liID'+idNumber;
 
     // Create new input for the checkbox.
     let checkbox = document.createElement('input');
-
     // Set the input's type to checkbox.
     checkbox.type = 'checkbox';
+
+    // Create the delete button.
+    let deleteButton = document.createElement('button');
+    // Set the delete button's type, text content and ID.
+    deleteButton.type = 'button';
+    deleteButton.textContent = 'delete';
+    deleteButton.id = 'deleteButtonID' + idNumber;
+    deleteButton.addEventListener("click", function(){
+      deleteParent(deleteButton.id);
+      }
+    );
 
     // Set the title.
     newLi.textContent = title;
@@ -28,13 +40,29 @@
     // Attach the textbox to the li.
     newLi.appendChild(checkbox);
 
+    // Attach the delete button
+    newLi.appendChild(deleteButton);
     // Empty the input.
     newToDoText.value = '';
 
+    // Increment the ID counter
+    ++idNumber;
+
+    // Function to delete the parent of the delete button
+    function deleteParent(deleteButtonID){
+
+      // Get the delete button object
+      var deleteButton = document.getElementById(deleteButtonID);
+      // Use the delete button to get "li" parent element
+      var parentLi = deleteButton.parentNode;
+      // Remove the parent "li"
+      parentLi.remove();
+
+    }
   });
 }
 
 window.onload = function(){
-  alert('New page has loaded!');
-  onReady();
+    alert('New page has loaded!');
+    onReady();
 };
