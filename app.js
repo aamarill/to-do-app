b24b82106896761f8c1db6f73d738dc17935178d
Adan Amarillas <aamarill.engr@gmail.com>
2017-09-22 14:55:58 -0700

    Add delete feature

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function onReady() {
     var newToDoText = document.getElementById('newToDoText');
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
     });
     newToDoText.value = ''; // empty out the input textbox
 
@@ -14,18 +14,31 @@ function onReady() {
   }
 
   function renderTheUI(toDos){
+    function deleteEntry(deleteButtonID){
+      var deleteButtonPressed = document.getElementById(deleteButtonID);
+      var liToBeRemoved = deleteButtonPressed.parentNode;
+      liToBeRemoved.remove();
+    };
+
     var toDoList = document.getElementById('toDoList');
     toDoList.innerHTML = '';
+    var IDs =1;
 
     toDos.forEach(function(toDo){
       var newLi    = document.createElement('li');
       var checkbox = document.createElement('input');
       checkbox.type = "checkbox";
+      var deleteButton = document.createElement('button');
+      deleteButton.innerHTML = 'delete';
+      deleteButton.id = 'id'+IDs;
+      ++IDs;
+      deleteButton.addEventListener("click", function(){deleteEntry(deleteButton.id)});
 
       newLi.innerHTML = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
 
     });
   }
